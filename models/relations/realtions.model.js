1e2815cc05d2f05e9094099ba3a7114df7757b2d
Nadjitan Betan <nadjitanb@gmail.com>
2026-03-08 18:09:37 +0000

    chore(model) add relation between user - hotel

--- models/relations/realtions.model.js
+++ models/relations/realtions.model.js
@@ -5,6 +5,9 @@ const User = require("../tables/user.model");
 Country.hasMany(Hotel, { foreignKey: "countryId", as: "hotels" })
 Hotel.belongsTo(Country, { foreignKey: "countryId", as: "country" })
 
+Hotel.hasMany(User, { foreignKey: "hotelId", as: "hotel" })
+User.belongsTo(Hotel, { foreignKey: "hotelId", as: "users" })
+
 Country.hasMany(User, { foreignKey: "countryId", as: "delegation" })
 User.belongsTo(Country, { foreignKey: "countryId", as: "country" })
 
